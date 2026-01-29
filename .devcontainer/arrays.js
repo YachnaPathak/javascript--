d2e4dff5628e3f580c54745a2596225f1139200f
YachnaPathak <yachnapathak2616@gmail.com>
2026-01-29 17:24:48 +0000

    Learned Number methods and precision formatting

---
+++ .devcontainer/arrays.js
@@ -0,0 +1,7 @@
+/*const balance=new Number(100)
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));*/
+const anothernumber=123.83966 
+console.log(anothernumber.toPrecision(5));
