5b4910452b0a4158c2715aaeb9a1e8ed1e898a6a
YachnaPathak <yachnapathak2616@gmail.com>
2026-01-30 06:00:15 +0000

    Added number and math basics in arrays.js

--- .devcontainer/arrays.js
+++ .devcontainer/arrays.js
@@ -2,6 +2,17 @@
 console.log(balance);
 console.log(balance.toString());
 console.log(balance.toString().length);
-console.log(balance.toFixed(2));*/
+console.log(balance.toFixed(2))
 const anothernumber=123.83966 
 console.log(anothernumber.toPrecision(5));
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-In'));
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));*/
+console.log(Math.min(4,3,6,8));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
