227cb40723c570eb7e5be23cc2605a2aa90596f6
YachnaPathak <yachnapathak2616@gmail.com>
2026-01-30 06:48:37 +0000

    Added number and math basics in arrays.js

--- .devcontainer/arrays.js
+++ .devcontainer/arrays.js
@@ -11,8 +11,18 @@ console.log(Math);
 console.log(Math.abs(-4));
 console.log(Math.round(4.6));
 console.log(Math.ceil(4.6));
-console.log(Math.floor(4.6));*/
+console.log(Math.floor(4.6));
 console.log(Math.min(4,3,6,8));
 console.log(Math.random());
 console.log((Math.random()*10)+1);
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min))+min)
+let mydate=new Date()
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+console.log(typeof mydate);
+let mycreateddate=new Date(2023,0,13)
+console.log(mycreateddate.toDateString());*/
 
